5673419c4a7197d3325fbe98791739e3a5ca2bb1
arthurdev <arthurmartins.dev2025@gmail.com>
2026-02-28 18:39:08 -0300

    test: update db test to coverage all tables

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -8,6 +8,31 @@ describe("Database test suite", () => {
 		expect(result.value).toBe(1);
 	});
 
+	it("should have the users table", () => {
+		const result = db
+			.query(
+				"SELECT name FROM sqlite_master WHERE type='table' AND name='users'",
+			)
+			.get() as { name: string } | undefined;
+		expect(result).toBeDefined();
+		expect(result?.name).toBe("users");
+	});
+
+	it("table users should have correct columns", () => {
+		const result = db.query("PRAGMA table_info(users)").all() as Array<{
+			name: string;
+		}>;
+		const columnNames = result.map((row) => row.name);
+		expect(columnNames).toEqual([
+			"id",
+			"username",
+			"email",
+			"password_hash",
+			"created_at",
+			"updated_at",
+		]);
+	});
+
 	it("should have the expenses table", () => {
 		const result = db
 			.query(
@@ -23,8 +48,31 @@ describe("Database test suite", () => {
 			name: string;
 		}>;
 		const columnNames = result.map((row) => row.name);
-		expect(columnNames).toEqual(
-			expect.arrayContaining(["id", "category", "amount", "updated_at"]),
-		);
+		expect(columnNames).toEqual([
+			"id",
+			"user_id",
+			"name",
+			"amount",
+			"created_at",
+			"updated_at",
+		]);
+	});
+
+	it("should have the categories table", () => {
+		const result = db
+			.query(
+				"SELECT name FROM sqlite_master WHERE type='table' AND name = 'categories'",
+			)
+			.get() as { name: string } | undefined;
+		expect(result).toBeDefined();
+		expect(result?.name).toBe("categories");
+	});
+
+	it("table categories should have correct columns", () => {
+		const result = db.query("PRAGMA table_info(categories)").all() as Array<{
+			name: string;
+		}>;
+		const columnNames = result.map((row) => row.name);
+		expect(columnNames).toEqual(["id", "name"]);
 	});
 });

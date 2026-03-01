e1806ba3dc7ed31adddd6400f1cb89f45eedd7e2
arthurdev <arthurmartins.dev2025@gmail.com>
2026-03-01 11:30:46 -0300

    refactor: rename server.js to index.ts for maintain elysia pattern

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { fromTypes, openapi } from "@elysiajs/openapi";
+import { Elysia } from "elysia";
+import { users } from "./modules/users/users.plugin";
+
+const app = new Elysia()
+	.use(
+		openapi({
+			references: fromTypes(),
+		}),
+	)
+	.get("/", () => "Hello Elysia")
+	.use(users);
+
+export { app };
+app.listen(3000);
+
+console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}
+📙 docs: http://${app.server?.hostname}:${app.server?.port}/openapi`,
+);

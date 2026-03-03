d6def7905a600ae17ef454f89104113c7ac3ede0
arthurdev <arthurmartins.dev2025@gmail.com>
2026-03-03 11:08:20 -0300

    refactor: change name file to db.ts from index.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,10 +4,10 @@ const isTest = process.env.NODE_ENV === "test";
 
 let db: Database;
 if (isTest) {
-	console.log("\n🛢️  Test env: Using in-memory database");
-	db = new Database(":memory:");
+	console.log("\n🛢️  Test env: Using test.db");
+	db = new Database("./test.db");
 } else {
-	console.log("\n🛢️  Dev env: Using file-based database ");
+	console.log("\n🛢️  Dev env: Using dev.db ");
 	db = new Database("./dev.db");
 }
 

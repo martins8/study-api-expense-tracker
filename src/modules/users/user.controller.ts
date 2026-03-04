ec21b5e9bac8bef3a0bf4d5a47b2b7401dbe2423
arthurdev <arthurmartins.dev2025@gmail.com>
2026-03-04 10:42:35 -0300

    feat: adds signIn route

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,13 +1,13 @@
 import { Elysia, status } from "elysia";
 import { AuthModel } from "./user.dto";
-import { createUser } from "./user.service";
+import { signIn, signUp } from "./user.service";
 
 export const users = new Elysia({ prefix: "api/users" })
-	.use(AuthModel) // injeta os models
+	.use(AuthModel)
 	.post(
 		"/sign-up",
 		async ({ body }) => {
-			const result = await createUser(body);
+			const result = await signUp(body);
 			if (result.ok) {
 				return status(201, {
 					message: "User registration successful",
@@ -33,19 +33,47 @@ export const users = new Elysia({ prefix: "api/users" })
 		{
 			body: "signUp",
 			response: {
-				201: "signUpRes",
+				201: "sucessAuthRes",
 				400: "errorRes",
 				422: "errorRes",
 				500: "errorRes",
 			},
 		},
-	);
-/*
-	.post("/sign-in", ({ body }) => "teste", {
-		body: "signIn",
-		response: {
-			200: "signInRes",
-			401: "invalidCredentialsRes",
+	)
+	.post(
+		"/sign-in",
+		async ({ body }) => {
+			const result = await signIn(body);
+			if (result.ok) {
+				return status(200, {
+					message: "User authentication successful",
+					token: "fake",
+				});
+			}
+			const statusMap = {
+				invalid_credentials: status(401, {
+					message: "Invalid email or password",
+					code: 401,
+				}),
+				invalid_email_format: status(400, {
+					message: "Invalid email format",
+					code: 400,
+				}),
+				internal_error: status(500, {
+					message: "Internal server error",
+					code: 500,
+				}),
+			};
+
+			return statusMap[result.error];
 		},
-		
-	});*/
+		{
+			body: "signIn",
+			response: {
+				200: "sucessAuthRes",
+				400: "errorRes",
+				401: "errorRes",
+				500: "errorRes",
+			},
+		},
+	);

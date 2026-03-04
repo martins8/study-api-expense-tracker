a1981ef48626fa85c94186b3923e8e98e17b5dbc
arthurdev <arthurmartins.dev2025@gmail.com>
2026-03-04 09:01:10 -0300

    feat: create user repository

---
+++ src/modules/users/user.repository.ts
@@ -0,0 +1,26 @@
+import { db } from "@/infra/db";
+
+export interface InsertUserData {
+	id: string;
+	email: string;
+	username: string;
+	password_hash: string;
+}
+
+type UserIdResult = {
+	id: string;
+} | null;
+
+export function insertUser(data: InsertUserData): void {
+	const { id, email, username, password_hash } = data;
+	db.prepare(
+		"INSERT INTO users (id, email, username, password_hash) VALUES (?, ?, ?, ?)",
+	).run(id, email, username, password_hash);
+}
+
+export function getUserIdByEmail(email: string): string | null {
+	const result = db
+		.prepare("SELECT id FROM users WHERE email = ?")
+		.get(email) as UserIdResult;
+	return result ? result.id : null;
+}
